12611ccee79e86502280aabfee681fa5c83c9962
Nithrone <kadir.dokur@bil.omu.edu.tr>
2023-11-15 12:37:45 +0300

    modified:   class-object-attribute-constructor.py

--- class-object-attribute-constructor.py
+++ class-object-attribute-constructor.py
@@ -12,9 +12,14 @@ class Person:
         self.name = name
         self.year = year
         
-        
-        print('init method is working')
+    # instance methods
+    def intro(self):
+        print('Hello, I am ' + self.name)
 
+
+    def calculateAge(self):
+        return 2023 - self.year
+    
 # object (instance)
 
 p1 = Person(name = 'Piotr', year = 1990)
@@ -27,3 +32,9 @@ p2.name = 'Reaper'
 #accessing object attributes
 print(f'p1 :name: {p1.name} year: {p1.year} address : {p1.address}')
 print(f'p1 :name: {p2.name} year: {p2.year} address : {p2.address}')
+
+p1.intro()
+p2.intro()
+
+print(f'Name: {p1.name} , Age:{p1.calculateAge()}')
+print(f'Name: {p2.name} , Age:{p2.calculateAge()}')
