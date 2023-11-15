8cc2b1a2b49cbb285d4f66deefbd38287009ad8b
Nithrone <kadir.dokur@bil.omu.edu.tr>
2023-11-15 12:16:53 +0300

    modified:   a.py

--- a.py
+++ a.py
@@ -1,2 +1,29 @@
-print("hello world")
-print("hello world")
+# Object Oriented Programming (OOP)
+
+# class
+
+class Person:
+    # class attributes
+    address = 'no information'
+
+    #constructor
+    def __init__(self, name, year):
+        # object attributes
+        self.name = name
+        self.year = year
+        
+        
+        print('init method is working')
+
+# object (instance)
+
+p1 = Person(name = 'Piotr', year = 1990)
+p2 = Person(name = 'Celil', year = 1995)
+
+# updating
+p1.name = 'Tobias'
+p2.name = 'Reaper'
+
+#accessing object attributes
+print(f'p1 :name: {p1.name} year: {p1.year} address : {p1.address}')
+print(f'p1 :name: {p2.name} year: {p2.year} address : {p2.address}')
